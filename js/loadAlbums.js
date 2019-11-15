1d397cf0ff7aa6c500bc31c6691b7a6f744f8102
Alexander <mrreads@yandex.com>
2019-11-16 00:06:34 +0300

    added page, with empty TRACK/ALBUM

--- js/loadAlbums.js
+++ js/loadAlbums.js
@@ -15,13 +15,13 @@ xhr.onload = () =>
         if (element[3] == 'album')
         {
             id = element[2];
-            link = '<iframe style="border: 0; width: 350px; height: 588px;" src="https://bandcamp.com/EmbeddedPlayer/album='+id+'/size=large/bgcol=ffffff/linkcol=de270f/transparent=true/" seamless> </iframe>';
+            link = '<iframe style="border: 0; width: 400px; height: 400px;" src="https://bandcamp.com/EmbeddedPlayer/album='+id+'/size=large/bgcol=ffffff/linkcol=0687f5/artwork=small/transparent=true/" seamless> </iframe>';
             document.querySelector("#albums").innerHTML += link;
         }
         if (element[3] == 'track')
         {
             id = element[2];
-            link = '<iframe style="border: 0; width: 350px; height: 442px;" src="https://bandcamp.com/EmbeddedPlayer/track='+id+'/size=large/bgcol=ffffff/linkcol=0687f5/tracklist=false/transparent=true/" seamless> </iframe>'
+            link = '<iframe style="border: 0; width: 308px; height: 400px;" src="https://bandcamp.com/EmbeddedPlayer/track='+id+'/size=large/bgcol=ffffff/linkcol=0687f5/tracklist=false/transparent=true/" seamless> </iframe>'
             document.querySelector("#albums").innerHTML += link;
         }
     });
--- php/addAlbum.php
+++ php/addAlbum.php
@@ -2,52 +2,76 @@
     include_once('../lib/simple_html_dom.php');
 
     $link = $_GET['link'];
-
-    if (strpos($link, 'album') == true) 
+    if (strpos($link, 'bandcamp') == true)
     {
-        $regex = '/(?<!href=["\'])http:\/\//';
-        $link = preg_replace('#^http?://#', '', $link);
-        $link = preg_replace('#^https?://#', '', $link);
-        $link = preg_replace('#^www.#', '', $link);
-    
-        $html = file_get_html('https://' . $link);
-    
-        $json = file_get_contents( __DIR__ . './../data.json');
-        $data = json_decode($json, true);
+        if (strpos($link, 'album') == true) 
+        {
+            $regex = '/(?<!href=["\'])http:\/\//';
+            $link = preg_replace('#^http?://#', '', $link);
+            $link = preg_replace('#^https?://#', '', $link);
+            $link = preg_replace('#^www.#', '', $link);
         
-        $idAlbum = $html->find('comment', -1);
-        $idAlbum = preg_replace("/[^0-9]/", '', $idAlbum);
-        $artist = explode('.bandcamp.com/album/', $link)[0];
-        $album = explode('.bandcamp.com/album/', $link)[1];
-    
-        $info = [$artist, $album, $idAlbum, "album"];
-        array_push($data, $info);
-    
-        $data = json_encode($data);
-        $json = file_put_contents( __DIR__ . './../data.json', $data);
-    }
-    if (strpos($link, 'track') == true) 
-    {
-        $regex = '/(?<!href=["\'])http:\/\//';
-        $link = preg_replace('#^http?://#', '', $link);
-        $link = preg_replace('#^https?://#', '', $link);
-        $link = preg_replace('#^www.#', '', $link);
-    
-        $html = file_get_html('https://' . $link);
-    
-        $json = file_get_contents( __DIR__ . './../data.json');
-        $data = json_decode($json, true);
+            $html = file_get_html('https://' . $link);
         
-        $idAlbum = $html->find('comment', -1);
-        $idAlbum = preg_replace("/[^0-9]/", '', $idAlbum);
-        $artist = explode('.bandcamp.com/track/', $link)[0];
-        $track = explode('.bandcamp.com/track/', $link)[1];
-    
-        $info = [$artist, $track, $idAlbum, "track"];
-        array_push($data, $info);
-    
-        $data = json_encode($data);
-        $json = file_put_contents( __DIR__ . './../data.json', $data);
-    }
+            $json = file_get_contents( __DIR__ . './../data.json');
+            $data = json_decode($json, true);
+            
+            $idAlbum = $html->find('comment', -1);
+            $idAlbum = preg_replace("/[^0-9]/", '', $idAlbum);
+            $artist = explode('.bandcamp.com/album/', $link)[0];
+            $album = explode('.bandcamp.com/album/', $link)[1];
+        
+            $info = [$artist, $album, $idAlbum, "album"];
+            array_push($data, $info);
+        
+            $data = json_encode($data);
+            $json = file_put_contents( __DIR__ . './../data.json', $data);
+        }
+        else if (strpos($link, 'track') == true) 
+        {
+            $regex = '/(?<!href=["\'])http:\/\//';
+            $link = preg_replace('#^http?://#', '', $link);
+            $link = preg_replace('#^https?://#', '', $link);
+            $link = preg_replace('#^www.#', '', $link);
+        
+            $html = file_get_html('https://' . $link);
+        
+            $json = file_get_contents( __DIR__ . './../data.json');
+            $data = json_decode($json, true);
+            
+            $idAlbum = $html->find('comment', -1);
+            $idAlbum = preg_replace("/[^0-9]/", '', $idAlbum);
+            $artist = explode('.bandcamp.com/track/', $link)[0];
+            $track = explode('.bandcamp.com/track/', $link)[1];
+        
+            $info = [$artist, $track, $idAlbum, "track"];
+            array_push($data, $info);
+        
+            $data = json_encode($data);
+            $json = file_put_contents( __DIR__ . './../data.json', $data);
+        }
+        else
+        {
+            $regex = '/(?<!href=["\'])http:\/\//';
+            $link = preg_replace('#^http?://#', '', $link);
+            $link = preg_replace('#^https?://#', '', $link);
+            $link = preg_replace('#^www.#', '', $link);
+        
+            $html = file_get_html('https://' . $link);
+        
+            $json = file_get_contents( __DIR__ . './../data.json');
+            $data = json_decode($json, true);
+            
+            $idAlbum = $html->find('comment', -1);
+            $idAlbum = preg_replace("/[^0-9]/", '', $idAlbum);
+            $artist = explode('.bandcamp.com/', $link)[0];
+        
+            $info = [$artist, '', $idAlbum, "album"];
+            array_push($data, $info);
+        
+            $data = json_encode($data);
+            $json = file_put_contents( __DIR__ . './../data.json', $data);
+        }
+    } 
     header('Location: ./../index.html');
 ?>
