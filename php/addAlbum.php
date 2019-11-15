d13ef94ee2d5c2a13cb7a890f233f6443131a150
Alexander <mrreads@yandex.com>
2019-11-16 00:23:17 +0300

    delete unused code

--- php/addAlbum.php
+++ php/addAlbum.php
@@ -50,28 +50,6 @@
             $data = json_encode($data);
             $json = file_put_contents( __DIR__ . './../data.json', $data);
         }
-        else
-        {
-            $regex = '/(?<!href=["\'])http:\/\//';
-            $link = preg_replace('#^http?://#', '', $link);
-            $link = preg_replace('#^https?://#', '', $link);
-            $link = preg_replace('#^www.#', '', $link);
-        
-            $html = file_get_html('https://' . $link);
-        
-            $json = file_get_contents( __DIR__ . './../data.json');
-            $data = json_decode($json, true);
-            
-            $idAlbum = $html->find('comment', -1);
-            $idAlbum = preg_replace("/[^0-9]/", '', $idAlbum);
-            $artist = explode('.bandcamp.com/', $link)[0];
-        
-            $info = [$artist, '', $idAlbum, "album"];
-            array_push($data, $info);
-        
-            $data = json_encode($data);
-            $json = file_put_contents( __DIR__ . './../data.json', $data);
-        }
     } 
     header('Location: ./../index.html');
 ?>
