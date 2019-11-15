f8e5b90b8b48ddf093f13f156c49cd2cd5acaa24
Alexander <mrreads@yandex.com>
2019-11-15 23:44:59 +0300

    add loading album | start for added track

--- js/loadAlbums.js
+++ js/loadAlbums.js
@@ -4,16 +4,25 @@ xhr.send();
 xhr.onload = () => 
 {
     let data = JSON.parse(xhr.response);
-    data = data.music;
 
+    let link;
     let artist;
     let album;
-    let link;
-    let albumID;
+    let id;
+
     data.forEach(element => 
     {
-        albumID = element[2];
-        link = '<iframe style="border: 0; width: 350px; height: 588px;" src="https://bandcamp.com/EmbeddedPlayer/album='+albumID+'/size=large/bgcol=ffffff/linkcol=de270f/transparent=true/" seamless> </iframe>';
-        document.querySelector("#albums").innerHTML += link;
+        if (element[3] == 'album')
+        {
+            id = element[3];
+            link = '<iframe style="border: 0; width: 350px; height: 588px;" src="https://bandcamp.com/EmbeddedPlayer/album='+id+'/size=large/bgcol=ffffff/linkcol=de270f/transparent=true/" seamless> </iframe>';
+            document.querySelector("#albums").innerHTML += link;
+        }
+        if (element[3] == 'track')
+        {
+            id = element[3];
+            link = '<iframe style="border: 0; width: 350px; height: 442px;" src="https://bandcamp.com/EmbeddedPlayer/track='+id+'/size=large/bgcol=ffffff/linkcol=0687f5/tracklist=false/transparent=true/" seamless> </iframe>'
+            document.querySelector("#albums").innerHTML += link;
+        }
     });
 };
---
+++ php/addAlbum.php
@@ -0,0 +1,30 @@
+<?php
+    include_once('../lib/simple_html_dom.php');
+
+    $link = $_GET['link'];
+
+    if (strpos($link, 'album') == true) 
+    {
+        $regex = '/(?<!href=["\'])http:\/\//';
+        $link = preg_replace('#^http?://#', '', $link);
+        $link = preg_replace('#^https?://#', '', $link);
+        $link = preg_replace('#^www.#', '', $link);
+    
+        $html = file_get_html('https://' . $link);
+    
+        $json = file_get_contents( __DIR__ . './../data.json');
+        $data = json_decode($json, true);
+        
+        $idAlbum = $html->find('comment', -1);
+        $idAlbum = preg_replace("/[^0-9]/", '', $idAlbum);
+        $artist = explode('.bandcamp.com/album/', $link)[0];
+        $album = explode('.bandcamp.com/album/', $link)[1];
+    
+        $info = [$artist, $album, $idAlbum];
+        array_push($data, $info);
+    
+        $data = json_encode($data);
+        $json = file_put_contents( __DIR__ . './../data.json', $data);
+    }
+    header('Location: ./../index.html');
+?>
