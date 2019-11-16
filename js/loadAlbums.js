93bfebf6e6b32764b71c3512e1dd88194a548a45
Alexander <mrreads@yandex.com>
2019-11-16 22:04:30 +0300

    start adaptive

--- js/loadAlbums.js
+++ js/loadAlbums.js
@@ -1,3 +1,5 @@
+let canseled;
+
 var xhr = new XMLHttpRequest();
 xhr.open('GET', './data.json', true);
 xhr.send();
@@ -5,24 +7,36 @@ xhr.onload = () =>
 {
     let data = JSON.parse(xhr.response);
 
-    let link;
     let id;
+    let iframe;
 
     data.forEach(element => 
     {
-        console.log(element);
-        
         if (element[3] == 'album')
         {
             id = element[2];
-            link = '<iframe style="border: 0; width: 300px; height: 300px;" src="https://bandcamp.com/EmbeddedPlayer/album='+id+'/size=large/bgcol=333333/linkcol=e32c14/minimal=true/transparent=true/" seamless> </iframe>';
-            document.querySelector("#albums").innerHTML += link;
+            iframe = document.createElement('iframe');
+            iframe.style = 'border: 0; height: 300px; width: 300px';
+            iframe.src = 'https://bandcamp.com/EmbeddedPlayer/album='+id+'/size=large/bgcol=333333/linkcol=e32c14/minimal=true/transparent=true/';
+            document.querySelector("#albums").appendChild(iframe);
         }
         if (element[3] == 'track')
         {
             id = element[2];
-            link = '<iframe style="border: 0; width: 300px; height: 300px;" src="https://bandcamp.com/EmbeddedPlayer/track='+id+'/size=large/bgcol=333333/linkcol=e32c14/minimal=true/transparent=true/" seamless> </iframe>'
-            document.querySelector("#albums").innerHTML += link;
+            iframe = document.createElement('iframe');
+            iframe.style = 'border: 0; height: 300px; width: 300px';
+            iframe.src = 'https://bandcamp.com/EmbeddedPlayer/track='+id+'/size=large/bgcol=333333/linkcol=e32c14/minimal=true/transparent=true/';
+            document.querySelector("#albums").appendChild(iframe);
         }
+    });
+};
+
+let allAlbums;
+window.onresize = () =>
+{
+    allAlbums = document.querySelectorAll('iframe');
+    allAlbums.forEach(element =>
+    {
+        element.style.height = element.offsetWidth + 'px';
     });
 };
